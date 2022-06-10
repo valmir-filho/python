cbc6cbbc8c8eb79c3279871183cde0eddda310cf
Valmir <vmcfilho@gmail.com>
2022-06-10 16:16:41 -0300

    Add files via upload

---
+++ Curso em Vídeo/aula4/aula4desa2.py
@@ -0,0 +1,7 @@
+# Crie um script Python que leia o dia, mês e o ano de nascimento de uma pessoa e mostre uma mensagem com
+# a data formatada.
+
+dia = int(input('Digite o dia do seu aniversário: '))
+mes = int(input('Digite o mês do seu aniversário: '))
+ano = int(input('Digite o ano do seu aniversário: '))
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano)
