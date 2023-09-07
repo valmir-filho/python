a298f26d4032d4469074510070516d0ce0abe10f
Valmir <vmcfilho@gmail.com>
2023-09-07 19:25:32 -0300

    Add files via upload

---
+++ python-3-completo-udemy/aula2.py
@@ -0,0 +1,13 @@
+# Método print
+
+# Exemplos
+
+print(12, 34)
+
+# Por padrão, o método print gera um espaço entre os 2 argumentos. Para alterar isso, usamos: sep=""
+print(12, 34, sep="")
+print(12, 34, sep="-")
+
+# Por padrão, o método print gera uma nova linha após a execução do comando. Podemos alterar isso usando: end=""
+print(12, 34, sep="", end="")
+print(56, 78, sep="")
