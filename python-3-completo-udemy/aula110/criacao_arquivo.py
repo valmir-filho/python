8c54203b1db0ff0ff8f4290046c1a387acfd324c
Valmir <vmcfilho@gmail.com>
2023-10-17 20:16:22 -0300

    Add files via upload

---
+++ python-3-completo-udemy/aula110/criacao_arquivo.py
@@ -0,0 +1,28 @@
+"""
+Manipulação de arquivos no Python.
+
+A manipulação de arquivos no Python é uma tarefa comum em programação, pois permite que você leia, escreva e gerencie dados armazenados em arquivos. O Python oferece várias bibliotecas e métodos embutidos para trabalhar com arquivos.
+"""
+
+"""
+Criação de um arquivo no Python.
+
+Para trabalhar com um arquivo, você primeiro precisa criá-lo usando a função "open()". Esta função recebe dois argumentos: o nome do arquivo e o modo de abertura (criação, leitura, escrita, etc.).
+
+Os modos de abertura mais comuns são:
+
+"w" para escrita (cria um novo arquivo ou sobrescreve o arquivo existente)
+"r" para leitura (padrão)
+"a" para anexar (adicionar dados ao final do arquivo)
+"x" para criar um novo arquivo para escrita, falhando se o arquivo já existir
+"b" para abrir o arquivo em modo binário (por exemplo, "rb" para leitura binária)
+"""
+
+arquivo = open(
+    "/Users/valmirfilho/Downloads/python-3-completo-udemy/aula110/arquivo.txt", "w")
+arquivo.close()
+
+# Dessa forma não há necessidade de fechar o arquivo
+# with open("/Users/valmirfilho/Downloads/python-3-completo-udemy/aula110/arquivo.txt", "w") as arquivo:
+#     print("Olá Mundo!")
+#     print("Arquivo será fechado!")
---
+++ python-3-completo-udemy/aula110/leitura_arquivo.py
@@ -0,0 +1,15 @@
+# Leitura de um arquivo no Python.
+
+arquivo = open(
+    "/Users/valmirfilho/Downloads/python-3-completo-udemy/aula110/arquivo.txt", "r")
+
+# conteudo_completo_arquivo = arquivo.read()
+# print(conteudo_completo_arquivo)
+
+# conteudo_unica_linha_arquivo = arquivo.readline()
+# print(conteudo_unica_linha_arquivo)
+
+# conteudo_multiplas_linhas_arquivo = arquivo.readlines()
+# print(conteudo_multiplas_linhas_arquivo)
+
+arquivo.close()
