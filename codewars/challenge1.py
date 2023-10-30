762410cef270166a635d3e4cbc2e0b2b7dc1dc64
Valmir <vmcfilho@gmail.com>
2023-10-30 09:39:40 -0300

    Delete codewars directory

--- codewars/challenge1.py
+++
@@ -1,8 +0,0 @@
-# Code as fast as you can! You need to double the integer and return it.
-
-
-def double_integer(i):
-    return i * 2
-
-
-double_integer(2)
--- codewars/challenge3.py
+++
@@ -1,8 +0,0 @@
-# Write a function which converts the input string to uppercase.
-
-
-def make_upper_case(s):
-    return s.upper()
-
-
-make_upper_case("test")
