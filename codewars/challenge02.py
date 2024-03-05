b838da3835edbc6a9cbd0e591c2f1946e0660685
Valmir <vmcfilho@gmail.com>
2024-03-05 11:25:11 -0300

    Create challenge02.py

---
+++ codewars/challenge02.py
@@ -0,0 +1,21 @@
+"""
+In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+Examples (Input -> Output)
+* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+* [9, 2, 0, 7]  -> [7, 0, 2, 9]
+"""
+
+
+def reverse_list(lst):
+    return lst[::-1]
+
+# Examples of use:
+
+example1 = [1, 2, 3, 4]
+result1 = reverse_list(example1)
+print(result1)  # Expected output: [4, 3, 2, 1]
+
+example2 = [9, 2, 0, 7]
+result2 = reverse_list(example2)
+print(result2)  # Expected output: [7, 0, 2, 9]
