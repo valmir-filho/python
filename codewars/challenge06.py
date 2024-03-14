ef05c7cf767b9715b37b39f6eb8ba5dedb7811f7
Valmir <vmcfilho@gmail.com>
2024-03-14 17:44:30 -0300

    Update challenge06.py

--- codewars/challenge06.py
+++ codewars/challenge06.py
@@ -1,5 +1,6 @@
 """
-Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition. The binary number returned should be a string.
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after
+the addition. The binary number returned should be a string.
 
 Examples: (Input1, Input2 --> Output (explanation)))
 
