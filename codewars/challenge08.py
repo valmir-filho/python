5ac02909bcd9aaad8adba52c202b40db26dc1f94
Valmir <vmcfilho@gmail.com>
2024-03-14 17:43:35 -0300

    Update challenge08.py

--- codewars/challenge08.py
+++ codewars/challenge08.py
@@ -1,5 +1,6 @@
 """
-Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the
+same value next to each other and preserving the original order of elements.
 
 Examples:
 
