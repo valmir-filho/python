98435ce7187167bf85d9b513f5fb916d00bac8c8
Valmir <vmcfilho@gmail.com>
2024-03-19 12:11:12 -0300

    Update challenge11.py

--- codewars/challenge11.py
+++ codewars/challenge11.py
@@ -1,7 +1,7 @@
 """
 Write a function that returns a string in which firstname is swapped with last name.
 
-Example(Input --> Output)
+Example (Input --> Output):
 "john McClane" --> "McClane john"
 """
 
