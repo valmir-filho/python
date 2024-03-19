fb11eba450dc8b8b76b8a3bfa8803ffd9dc6e70a
Valmir <vmcfilho@gmail.com>
2024-03-19 11:56:24 -0300

    Update challenge01.py

--- codewars/challenge01.py
+++ codewars/challenge01.py
@@ -5,7 +5,7 @@ every word. For simplicity, you'll have to capitalize each word, check out how c
 Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they
 are not capitalized in the same way he originally typed them.
 
-Example:
+Examples:
 Not Jaden-Cased: "How can mirrors be real if our eyes aren't real."
 Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real."
 """
