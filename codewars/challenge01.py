3fd05ec0c1cb9080f554e75cc35d7ba522557489
Valmir <vmcfilho@gmail.com>
2024-03-19 12:00:41 -0300

    Update challenge01.py

--- codewars/challenge01.py
+++ codewars/challenge01.py
@@ -6,8 +6,8 @@ Your task is to convert strings to how they would be written by Jaden Smith. The
 are not capitalized in the same way he originally typed them.
 
 Examples:
-Not Jaden-Cased: "How can mirrors be real if our eyes aren't real."
-Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real."
+1) Not Jaden-Cased: "How can mirrors be real if our eyes aren't real."
+2) Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real."
 """
 
 
