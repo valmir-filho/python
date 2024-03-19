7105c8833e85e628822bbcc1edbfdaa736c075ef
Valmir <vmcfilho@gmail.com>
2024-03-19 12:01:16 -0300

    Update challenge02.py

--- codewars/challenge02.py
+++ codewars/challenge02.py
@@ -2,8 +2,8 @@
 In this kata you will create a function that takes in a list and returns a list with the reverse order.
 
 Examples (Input -> Output):
-* [1, 2, 3, 4]  -> [4, 3, 2, 1]
-* [9, 2, 0, 7]  -> [7, 0, 2, 9]
+1) [1, 2, 3, 4]  -> [4, 3, 2, 1]
+2) [9, 2, 0, 7]  -> [7, 0, 2, 9]
 """
 
 
