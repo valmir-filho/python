f2072d1e261ca453bb10bbae1fcbc7725954c7ae
Valmir <vmcfilho@gmail.com>
2024-03-19 12:01:46 -0300

    Update challenge03.py

--- codewars/challenge03.py
+++ codewars/challenge03.py
@@ -2,7 +2,6 @@
 In this kata, you are asked to square every digit of a number and concatenate them.
 
 Examples:
-
 1) If we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1 (81-1-1-81).
 2) An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25 (49-36-25).
 
