bedd7c5f84778f8315a9f1f8ee38651bb2a3628e
Valmir <vmcfilho@gmail.com>
2024-03-19 12:06:51 -0300

    Update challenge10.py

--- codewars/challenge10.py
+++ codewars/challenge10.py
@@ -1,10 +1,9 @@
 """
-Instructions
 Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, and returns
 an ordered list containing the indices of all capital (uppercase) letters in the string.
 
-Example (Input --> Output)
-"CodEWaRs" --> [0,3,4,6]
+Example (Input --> Output):
+1) "CodEWaRs" --> [0,3,4,6]
 """
 
 
