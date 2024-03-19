e73f48e1308ee813a7d85651413a21362a4e2556
Valmir <vmcfilho@gmail.com>
2024-03-19 12:06:00 -0300

    Update challenge08.py

--- codewars/challenge08.py
+++ codewars/challenge08.py
@@ -3,11 +3,10 @@ Implement the function unique_in_order which takes as argument a sequence and re
 same value next to each other and preserving the original order of elements.
 
 Examples:
-
-unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
-unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
-unique_in_order([1, 2, 2, 3, 3])   == [1, 2, 3]
-unique_in_order((1, 2, 2, 3, 3))   == [1, 2, 3]
+1) unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B'].
+2) unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D'].
+3) unique_in_order([1, 2, 2, 3, 3])   == [1, 2, 3].
+4) unique_in_order((1, 2, 2, 3, 3))   == [1, 2, 3].
 """
 
 
