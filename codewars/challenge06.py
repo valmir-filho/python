660772c01bc046d50517d6e5a602f9917470d28c
Valmir <vmcfilho@gmail.com>
2024-03-19 12:04:05 -0300

    Update challenge06.py

--- codewars/challenge06.py
+++ codewars/challenge06.py
@@ -2,10 +2,9 @@
 Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after
 the addition. The binary number returned should be a string.
 
-Examples: (Input1, Input2 --> Output (explanation)))
-
-1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
-5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+Examples: (Input1, Input2 --> Output (explanation)).
+1) 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary).
+2) 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary).
 """
 
 
