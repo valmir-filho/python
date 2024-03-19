271e70e14a3bc2cefa5e4361195f57aa4e4de74e
Valmir <vmcfilho@gmail.com>
2024-03-19 12:07:08 -0300

    Update challenge04.py

--- codewars/challenge04.py
+++ codewars/challenge04.py
@@ -5,7 +5,7 @@ In this game, the hero moves from left to right. The player rolls the dice and m
 times. Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
 
 Example:
-move(3, 6) should equal 15.
+1) move(3, 6) should equal 15.
 """
 
 
