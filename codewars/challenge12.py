f12544c9cd024bad3f08627fc21580ccf9c5fcc5
Valmir <vmcfilho@gmail.com>
2024-03-19 12:13:18 -0300

    Update challenge12.py

--- codewars/challenge12.py
+++ codewars/challenge12.py
@@ -4,14 +4,14 @@ Given an array of ones and zeroes, convert the equivalent binary value to an int
 Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
 
 Examples:
-Testing: [0, 0, 0, 1] ==> 1
-Testing: [0, 0, 1, 0] ==> 2
-Testing: [0, 1, 0, 1] ==> 5
-Testing: [1, 0, 0, 1] ==> 9
-Testing: [0, 0, 1, 0] ==> 2
-Testing: [0, 1, 1, 0] ==> 6
-Testing: [1, 1, 1, 1] ==> 15
-Testing: [1, 0, 1, 1] ==> 11
+1) Testing: [0, 0, 0, 1] ==> 1.
+2) Testing: [0, 0, 1, 0] ==> 2.
+3) Testing: [0, 1, 0, 1] ==> 5.
+4) Testing: [1, 0, 0, 1] ==> 9.
+5) Testing: [0, 0, 1, 0] ==> 2.
+6) Testing: [0, 1, 1, 0] ==> 6.
+7) Testing: [1, 1, 1, 1] ==> 15.
+8) Testing: [1, 0, 1, 1] ==> 11.
 
 However, the arrays can have varying lengths, not just limited to 4.
 """
