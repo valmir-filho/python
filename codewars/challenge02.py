2f541ff62df1ebddc69a91a7c343f9bb769a1c87
Valmir <vmcfilho@gmail.com>
2024-03-20 11:16:37 -0300

    Update challenge02.py

--- codewars/challenge02.py
+++ codewars/challenge02.py
@@ -11,11 +11,9 @@ def reverse_list(lst):
     return lst[::-1]
 
 # Usage examples.
-
 example1 = [1, 2, 3, 4]
 result1 = reverse_list(example1)
 print(result1)  # Expected output: [4, 3, 2, 1].
-
 example2 = [9, 2, 0, 7]
 result2 = reverse_list(example2)
 print(result2)  # Expected output: [7, 0, 2, 9].
