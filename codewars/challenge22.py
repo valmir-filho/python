bfa506b2754a56a735fe345b49921044b06a82e2
Valmir <vmcfilho@gmail.com>
2024-03-26 15:58:49 -0300

    Create challenge22.py

---
+++ codewars/challenge22.py
@@ -0,0 +1,20 @@
+"""
+You will be given an array and a limit value. You must check that all values in the array
+are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+"""
+
+
+def check_array_against_limit(array, limit):
+    # Check each value in the array.
+    for value in array:
+        if value > limit:
+            return False
+    return True
+
+# Usagem example.
+array = [10, 20, 30, 40]
+limit = 50
+result = check_array_against_limit(array, limit)
+print("All values are within the limit:", result)
