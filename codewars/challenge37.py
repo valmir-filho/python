794559e6574e1041697489ecb8f4d9971077457b
Valmir <vmcfilho@gmail.com>
2024-05-01 16:30:47 -0300

    Create challenge37.py

---
+++ codewars/challenge37.py
@@ -0,0 +1,8 @@
+"""
+Write a function that removes the spaces from the string, then return the resultant string.
+"""
+
+
+def no_space(x):
+    result_string = x.replace(" ", "")
+    return result_string
