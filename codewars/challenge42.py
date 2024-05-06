37443cc4b1f689707fdc9b69099a7e3ac5add440
Valmir <vmcfilho@gmail.com>
2024-05-06 18:29:36 -0300

    Update challenge42.py

--- codewars/challenge42.py
+++ codewars/challenge42.py
@@ -1,6 +1,14 @@
 """
 Your classmates asked you to copy some paperwork for them.
 
-You know that there are 'n' classmates and the paperwork has 'm' pages.
+You know that there are "n" classmates and the paperwork has "m" pages.
 
 Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+"""
+
+
+def blank_pages_needed(n, m):
+    if n < 0 or m < 0:
+        return 0
+    else:
+        return n * m
