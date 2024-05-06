7b8fb23fae26093a1cdf32bf508bc7d544e84ba9
Valmir <vmcfilho@gmail.com>
2024-05-06 18:27:15 -0300

    Create challenge42.py

---
+++ codewars/challenge42.py
@@ -0,0 +1,6 @@
+"""
+Your classmates asked you to copy some paperwork for them.
+
+You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
