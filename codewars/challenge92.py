d0063d51e956e220a7fef4d555598786caf8c3bd
Valmir <vmcfilho@gmail.com>
2024-07-12 10:49:53 -0300

    Create challenge92.py

---
+++ codewars/challenge92.py
@@ -0,0 +1,15 @@
+"""
+Your goal in this kata is to implement a difference function,
+which subtracts one list from another and returns the result.
+It should remove all values from list a, which are present in
+list b keeping their order.
+
+array_diff([1,2],[1]) == [2]
+
+If a value is present in b, all of its occurrences must be
+removed from the other: array_diff([1,2,2,2,3],[2]) == [1,3]
+"""
+
+
+def array_diff(a, b):
+    return [item for item in a if item not in b]
