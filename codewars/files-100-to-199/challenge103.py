aeb155f276d108f36a5bb7ecbf483d7fda184293
Valmir <vmcfilho@gmail.com>
2024-07-23 11:40:54 -0300

    Create challenge103.py

---
+++ codewars/files-100-to-199/challenge103.py
@@ -0,0 +1,15 @@
+"""
+This time no story, no theory.
+
+The examples below show you how to write function accum:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+"""
+
+
+def accum(s):
+    return '-'.join((char * (i + 1)).capitalize() for i, char in enumerate(s))
+  
