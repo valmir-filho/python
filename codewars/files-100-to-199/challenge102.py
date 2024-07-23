a39d9620fd40175e5979ecabc652789f53cf3b7e
Valmir <vmcfilho@gmail.com>
2024-07-23 10:30:33 -0300

    Create challenge102.py

---
+++ codewars/files-100-to-199/challenge102.py
@@ -0,0 +1,10 @@
+"""
+In this little assignment you are given a string of
+space separated numbers, and have to return the highest
+and lowest number.
+"""
+
+
+def high_and_low(numbers):
+    num_list = [int(num) for num in numbers.split()]
+    return f"{max(num_list)} {min(num_list)}"
