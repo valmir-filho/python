a67b5d2c5fed0712afce540694fe67fd7fcfb397
Valmir <vmcfilho@gmail.com>
2024-08-25 12:31:13 -0300

    Create challenge138.py

---
+++ codewars/files-100-to-199/challenge138.py
@@ -0,0 +1,12 @@
+"""
+Debugging sayHello function
+
+The starship Enterprise has run into some problem when creating
+a program to greet everyone as they come aboard. It is your job
+to fix the code and get the program working again!
+"""
+
+
+def say_hello(name):
+    # Create the greeting message.
+    return f"Hello, {name}"
