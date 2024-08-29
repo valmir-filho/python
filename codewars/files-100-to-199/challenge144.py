f7fb145cb3b40d6583745835f500d474b10eab8f
Valmir <vmcfilho@gmail.com>
2024-08-29 11:00:45 -0300

    Create challenge144.py

---
+++ codewars/files-100-to-199/challenge144.py
@@ -0,0 +1,23 @@
+"""
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+if bmi <= 25.0 return "Normal"
+if bmi <= 30.0 return "Overweight"
+if bmi > 30 return "Obese"
+"""
+
+
+def bmi(weight, height):
+    # Calculate BMI.
+    bmi_value = weight / (height ** 2)
+    
+    # Determine the BMI category.
+    if bmi_value <= 18.5:
+        return "Underweight"
+    elif bmi_value <= 25.0:
+        return "Normal"
+    elif bmi_value <= 30.0:
+        return "Overweight"
+    else:
+        return "Obese"
