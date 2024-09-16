3cd1a85b0ffe682024ece955317bd533c602bb13
Valmir <vmcfilho@gmail.com>
2024-09-16 10:59:27 -0300

    Create challenge162.py

---
+++ codewars/files-100-to-199/challenge162.py
@@ -0,0 +1,16 @@
+"""
+Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+
+N! = 1 * 2 * 3 *  ... * N
+
+Be careful 1000! has 2568 digits...
+"""
+
+
+def zeros(n):
+    count = 0
+    power_of_5 = 5
+    while n >= power_of_5:
+        count += n // power_of_5
+        power_of_5 *= 5
+    return count
