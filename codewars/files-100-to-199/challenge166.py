a6f790d340d4867314e73bf253e55899a7a14b05
Valmir <vmcfilho@gmail.com>
2024-09-20 09:18:35 -0300

    Create challenge166.py

---
+++ codewars/files-100-to-199/challenge166.py
@@ -0,0 +1,11 @@
+"""
+Note: This kata is inspired by Convert a Number to a String! Try that one too.
+
+Description: We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+"""
+
+
+def string_to_number(s):
+    return int(s)
