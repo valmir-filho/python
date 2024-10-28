cfc92723e3a33a6760f428de633600c45202c551
Valmir <vmcfilho@gmail.com>
2024-10-28 08:40:58 -0300

    Create challenge200.py

---
+++ codewars/files-200-to-299/challenge200.py
@@ -0,0 +1,8 @@
+"""
+Given a 2D (nested) list (array, vector, ...) of size m * n, your task is to find the sum of the minimum values in each row.
+"""
+
+
+def sum_of_minimums(numbers):
+    # Use a list comprehension to find the minimum of each row and sum them.
+    return sum(min(row) for row in numbers)
