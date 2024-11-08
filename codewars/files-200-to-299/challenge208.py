8b933a546df9e59303b7d87816c541ca241f220b
Valmir <vmcfilho@gmail.com>
2024-11-08 08:30:21 -0300

    Create challenge208.py

---
+++ codewars/files-200-to-299/challenge208.py
@@ -0,0 +1,8 @@
+"""
+Replace all vowel to exclamation mark in the sentence.
+"""
+
+
+def replace_exclamation(st):
+    vowels = "aeiouAEIOU"
+    return ''.join('!' if char in vowels else char for char in st)
