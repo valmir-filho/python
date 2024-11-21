8121a238075f7467af986b37f1881f7fb22aa0d1
Valmir <vmcfilho@gmail.com>
2024-11-21 08:00:02 -0300

    Create challenge217.py

---
+++ codewars/files-200-to-299/challenge217.py
@@ -0,0 +1,13 @@
+"""
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+"""
+
+
+def get_char(c):
+    """
+    Takes a number and returns the corresponding ASCII character.
+    
+    :param c: Integer representing the ASCII value.
+    :return: Character corresponding to the ASCII value.
+    """
+    return chr(c)
