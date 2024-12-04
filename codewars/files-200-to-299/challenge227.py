0020023efdc7a666b76ab53f91e63a647ab8bef4
Valmir <vmcfilho@gmail.com>
2024-12-04 08:42:10 -0300

    Create challenge227.py

---
+++ codewars/files-200-to-299/challenge227.py
@@ -0,0 +1,13 @@
+"""
+You will be given an array a and a value x.
+All you need to do is check whether the provided
+array contains the value, without using a loop.
+
+Array can contain numbers or strings. x can be either.
+Return true if the array contains the value, false if not.
+With strings you will need to account for case.
+"""
+
+
+def check(a, x):
+    return x in a
