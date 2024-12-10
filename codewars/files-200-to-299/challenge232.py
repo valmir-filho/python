8226bcb5517778670527fe65a231ea97c6cbfb76
Valmir <vmcfilho@gmail.com>
2024-12-10 13:51:42 -0300

    Create challenge232.py

---
+++ codewars/files-200-to-299/challenge232.py
@@ -0,0 +1,7 @@
+"""
+Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+"""
+
+
+def take(arr, n):
+    return arr[:n]
