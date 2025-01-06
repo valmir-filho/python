47626d7d594c0b46417572929d64a5add6b7ab9e
Valmir <vmcfilho@gmail.com>
2025-01-06 08:52:30 -0300

    Create challenge251.py

---
+++ codewars/files-200-to-299/challenge251.py
@@ -0,0 +1,9 @@
+"""
+Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example: With "James" as the first name and "Stevens" as the last name should return "James Stevens"
+"""
+
+
+def combine_names(first_name, last_name):
+    return f"{first_name} {last_name}"
