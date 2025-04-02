8af6234f34c98bb434cc35222b9fe28b6b074fd6
Valmir <vmcfilho@gmail.com>
2025-04-02 09:04:07 -0300

    Create challenge282.py

---
+++ codewars/files-200-to-299/challenge282.py
@@ -0,0 +1,7 @@
+"""
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+The websites array has already been defined for you using the following code: websites = []
+"""
+
+websites.append("codewars")
