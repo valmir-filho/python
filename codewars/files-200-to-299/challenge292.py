026f75e2af721756948f18da3e5c50c738f94242
Valmir <vmcfilho@gmail.com>
2025-04-13 09:45:16 -0300

    Update challenge292.py

--- codewars/files-200-to-299/challenge292.py
+++ codewars/files-200-to-299/challenge292.py
@@ -1,1 +1,18 @@
-zzz
+"""
+Hey awesome programmer!
+
+You've got much data to manage and of course you use zero-based and non-negative ID's to make each data item unique!
+
+Therefore you need a method, which returns the smallest unused ID for your next new data item...
+
+Note: The given array of used IDs may be unsorted. For test reasons there may be duplicate IDs, but you don't have to find or remove them!
+
+Go on and code some pure awesomeness!
+"""
+
+
+def next_id(arr):
+    i = 0
+    while i in arr:
+        i += 1
+    return i
