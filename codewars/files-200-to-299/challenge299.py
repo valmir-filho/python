bfa639901c91c61b1ff58233945146919ea477e7
Valmir <vmcfilho@gmail.com>
2025-04-24 08:28:47 -0300

    Create challenge299.py

---
+++ codewars/files-200-to-299/challenge299.py
@@ -0,0 +1,17 @@
+"""
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+
+Note: if this seems too simple for you try the next level.
+
+Note2: the input data can be: boolean array, array of objects, array of string arrays, array of number arrays...😕.
+"""
+
+
+def print_array(arr):
+    return ','.join(map(str, arr))
