bdd5ad7fb1c5c490fc8f1a047df66c29a6426a06
Valmir <vmcfilho@gmail.com>
2025-04-27 14:41:01 -0300

    Atualizar o challenge300.py

--- codewars/files-300-to-399/challenge300.py
+++ codewars/files-300-to-399/challenge300.py
@@ -14,7 +14,7 @@ Your function would return the following array:
 ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
 
 The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed.
-Note that all of the strings will be in the same case as those provided, and some elements may be repeated
+Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
 """
 
 geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
