9c2a0df00a18c97cd28723fe2b057b2f6426fca1
Valmir <vmcfilho@gmail.com>
2025-05-03 14:33:04 -0300

    Update challenge303.py

--- codewars/files-300-to-399/challenge303.py
+++ codewars/files-300-to-399/challenge303.py
@@ -3,7 +3,7 @@ Write a function that accepts a square matrix (N x N 2D array) and returns the d
 
 How to take the determinant of a matrix -- it is simplest to start with the smallest cases:
 
-- A 1x1 matrix |a| has determinant a.
+- A 1x1 matrix |a| has determinant a;
 
 - A 2x2 matrix has determinant: a*d - b*c.
 
