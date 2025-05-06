7f4e6df2d5c4ae896f1e6672b27db7db8dcc99ae
Valmir <vmcfilho@gmail.com>
2025-05-06 08:00:23 -0300

    Create challenge304.py

---
+++ codewars/files-300-to-399/challenge304.py
@@ -0,0 +1,7 @@
+"""
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+"""
+
+
+def greet(name): 
+    return f"Hello {name.capitalize()}!"
