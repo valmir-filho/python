aaed68107f038e6867f852c9492d083acf802b47
Valmir <vmcfilho@gmail.com>
2025-05-26 08:29:33 -0300

    Create challenge324.py

---
+++ codewars/files-300-to-399/challenge324.py
@@ -0,0 +1,8 @@
+"""
+Each number should be formatted that it is rounded to two decimal places.
+You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+"""
+
+
+def two_decimal_places(n):
+    return round(n, 2)
