2099e9cb92d28bafa0d411aa6b0f22f26ec73184
Valmir <vmcfilho@gmail.com>
2025-05-30 11:06:18 -0300

    Create challenge328.py

---
+++ codewars/files-300-to-399/challenge328.py
@@ -0,0 +1,9 @@
+"""
+Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+If they are, change the array value to a string of that vowel.
+"""
+
+
+def is_vow(inp):
+    return [chr(i) if chr(i) in 'aeiou' else i for i in inp]
