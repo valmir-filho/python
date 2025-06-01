99c3b477a7b1e3875856f5255fc45622c9f954ec
Valmir <vmcfilho@gmail.com>
2025-06-01 18:25:49 -0300

    Atualizar o challenge328.py

--- codewars/files-300-to-399/challenge328.py
+++ codewars/files-300-to-399/challenge328.py
@@ -1,4 +1,4 @@
-"""
+""
 Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
 
 If they are, change the array value to a string of that vowel.
