79548a79f47b773637d8006d0319872b05b920c3
Valmir <vmcfilho@gmail.com>
2025-06-11 08:30:08 -0300

    Create challenge336.py

---
+++ codewars/files-300-to-399/challenge336.py
@@ -0,0 +1,35 @@
+"""
+Messi's Goal Total
+
+Use variables to find the sum of the goals Messi scored in 3 competitions.
+
+Information
+
+Messi goal scoring statistics:
+
+Competition	         Goals
+La Liga	             43
+Champions League     10
+Copa del Rey	       5
+
+Task
+
+Create these three variables and store the appropriate values using the table above:
+
+la_liga_goals
+champions_league_goals
+copa_del_rey_goals
+
+Create a fourth variable named total_goals that stores the sum of all of Messi's goals for this year.
+"""
+
+# Definir as variáveis com o número de gols em cada competição.
+la_liga_goals = 43
+champions_league_goals = 10
+copa_del_rey_goals = 5
+
+# Calcular o total de gols somando as variáveis.
+total_goals = la_liga_goals + champions_league_goals + copa_del_rey_goals
+
+# Exibir o total de gols.
+print(f"O total de gols de Messi é: {total_goals}")
