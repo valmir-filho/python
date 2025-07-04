b91b2b33cef62f1459f1224113ba49a591d1e825
Valmir <vmcfilho@gmail.com>
2025-07-04 08:08:05 -0300

    Create challenge343.py

---
+++ codewars/files-300-to-399/challenge343.py
@@ -0,0 +1,27 @@
+"""
+Color Ghost
+
+Create a class Ghost
+
+Ghost objects are instantiated without any arguments.
+
+Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated.
+
+ghost = Ghost()
+ghost.color  #=> "white" or "yellow" or "purple" or "red"
+"""
+
+import random
+
+
+class Ghost:
+    def __init__(self):
+        colors = ["white", "yellow", "purple", "red"]
+        self.color = random.choice(colors)
+
+# Example usage:
+ghost1 = Ghost()
+print(ghost1.color)
+
+ghost2 = Ghost()
+print(ghost2.color)
