1961d0d667454310daf3f452409b7c1927bcd37e
Valmir <vmcfilho@gmail.com>
2025-09-30 10:42:08 -0300

    Update challenge101.py

--- codewars/files-100-to-199/challenge101.py
+++ codewars/files-100-to-199/challenge101.py
@@ -1,4 +1,4 @@
-""
+"""
 Usually when you buy something, you're asked whether your credit card number,
 phone number or answer to your most secret question is still correct.
 However, since someone could look over your shoulder, you don't want that shown
