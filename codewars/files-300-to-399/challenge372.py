1d439cd40ad569a42f58969768e1431624d76dca
Valmir <vmcfilho@gmail.com>
2025-10-13 09:09:43 -0300

    Create challenge372.py

---
+++ codewars/files-300-to-399/challenge372.py
@@ -0,0 +1,6 @@
+"""
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+Add the value "codewars" to the array websites 1,000 times.
+"""
+
+websites = ["codewars"] * 1000
