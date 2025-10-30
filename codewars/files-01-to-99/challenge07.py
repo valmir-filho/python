819549d9f6504df570cebc94c4afd9a20bd712b2
Valmir <vmcfilho@gmail.com>
2025-10-30 08:04:50 -0300

    Update challenge07.py

--- codewars/files-01-to-99/challenge07.py
+++ codewars/files-01-to-99/challenge07.py
@@ -1,4 +1,4 @@
-""
+"""
 Write a function that checks if a given string (case insensitive) is a palindrome. A palindrome is a word, number, phrase, or other
 sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
 """
