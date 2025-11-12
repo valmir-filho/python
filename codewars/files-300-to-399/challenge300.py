f35c174a4a3593b9897ea2436a1b3a1a8f10567f
Valmir <vmcfilho@gmail.com>
2025-11-12 14:26:44 -0300

    Update challenge300.py

--- codewars/files-300-to-399/challenge300.py
+++ codewars/files-300-to-399/challenge300.py
@@ -1,4 +1,4 @@
-""
+"""
 Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
 
 The geese are any strings in the following array, which is pre-populated in your solution:
