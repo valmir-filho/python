301365a25105177cfad1b6c25e76dd05cb8f37ee
Valmir <vmcfilho@gmail.com>
2025-11-26 08:04:15 -0300

    Implement Leo's Oscar response function

---
+++ codewars/files-300-to-399/challenge381.py
@@ -0,0 +1,23 @@
+"""
+You have to write a function that describe Leo:
+
+def leo(oscar):
+  pass
+
+if oscar was (integer) 88, you have to return "Leo finally won the oscar! Leo is happy".
+if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
+if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?".
+if it was over 88 you should return "Leo got one already!".
+"""
+
+
+def leo(oscar):
+    if oscar == 88:
+        return "Leo finally won the oscar! Leo is happy"
+    elif oscar == 86:
+        return "Not even for Wolf of wallstreet?!"
+    elif oscar < 88:
+        return "When will you give Leo an Oscar?"
+    else:  # oscar > 88
+        return "Leo got one already!"
+    
