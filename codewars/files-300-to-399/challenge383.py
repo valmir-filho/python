2cee07dbe9ab88806908242c6ebf6f14157363f2
Valmir <vmcfilho@gmail.com>
2025-11-28 08:12:51 -0300

    Create challenge383.py

---
+++ codewars/files-300-to-399/challenge383.py
@@ -0,0 +1,27 @@
+"""
+Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+Example:
+
+input:
+   [[ 0, 1, 2, 3, 4 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]] 
+    
+output:
+     '0,1,2,3,4\n'
+    +'10,11,12,13,14\n'
+    +'20,21,22,23,24\n'
+    +'30,31,32,33,34'
+
+Array's length > 2.
+
+More details here: https://en.wikipedia.org/wiki/Comma-separated_values
+
+Note: you shouldn't escape the \n, it should work as a new line.
+"""
+
+
+def to_csv_text(array):
+    return "\n".join(",".join(str(num) for num in row) for row in array)
