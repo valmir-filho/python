c19a00ba3d6287002eccbfcdb461ae42445e0d0d
Valmir <vmcfilho@gmail.com>
2025-11-30 06:38:50 -0300

    Atualizar o challenge104.py

--- codewars/files-100-to-199/challenge104.py
+++ codewars/files-100-to-199/challenge104.py
@@ -1,4 +1,4 @@
-""
+"""
 Your job is to write a function which increments a string, to create a new string.
 If the string already ends with a number, the number should be incremented by 1.
 If the string does not end with a number. the number 1 should be appended to the new string.
