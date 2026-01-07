80c1f190da5236ae3d0fa9f985d68b9cb715de00
Valmir <vmcfilho@gmail.com>
2026-01-07 08:24:05 -0300

    Add function to round numbers to two decimal places
    Implement function to round numbers to two decimal places.

---
+++ codewars/files-400-to-499/challenge403.py
@@ -0,0 +1,13 @@
+"""
+Each number should be formatted that it is rounded to two decimal places.
+You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+Example:    
+
+5.5589 is rounded 5.56   
+-3.3424 is rounded -3.34
+"""
+
+
+def two_decimal_places(n):
+    return round(n, 2)
