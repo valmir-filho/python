2a897d3510b724aad61996f06093829842e48a8c
Valmir <vmcfilho@gmail.com>
2026-01-18 10:29:28 -0300

    Atualizar o challenge403.py

--- codewars/files-400-to-499/challenge403.py
+++ codewars/files-400-to-499/challenge403.py
@@ -1,4 +1,4 @@
-""
+"""
 Each number should be formatted that it is rounded to two decimal places.
 You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
 
