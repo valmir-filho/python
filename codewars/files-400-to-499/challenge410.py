f69de01abc900b518b05047b427095f270fe427b
Valmir <vmcfilho@gmail.com>
2026-01-22 09:03:36 -0300

    Add function to compute integral of polynomial
    Implement integrate function to calculate integral of polynomial expressions.

---
+++ codewars/files-400-to-499/challenge410.py
@@ -0,0 +1,27 @@
+"""
+Create a function that finds the integral of the expression passed.
+
+In order to find the integral all you need to do is add one to the exponent (the second argument),
+and divide the coefficient (the first argument) by that new number.
+
+For example for 3x^2, the integral would be 1x^3: we added 1 to the exponent, and divided the coefficient by that new number).
+
+Notes:
+
+The output should be a string.
+
+The coefficient and exponent is always a positive integer.
+
+Examples:
+ 3, 2  -->  "1x^3"
+12, 5  -->  "2x^6"
+20, 1  -->  "10x^2"
+40, 3  -->  "10x^4"
+90, 2  -->  "30x^3"
+"""
+
+
+def integrate(coefficient, exponent):
+    new_exponent = exponent + 1
+    new_coefficient = coefficient // new_exponent
+    return f"{new_coefficient}x^{new_exponent}"
