1281aded1e7001764250f33e358276c8a578c132
Valmir <vmcfilho@gmail.com>
2026-01-23 09:06:47 -0300

    Create challenge411.py

---
+++ codewars/files-400-to-499/challenge411.py
@@ -0,0 +1,39 @@
+"""
+Task:
+
+Suppose there are n people standing in a circle and they are numbered 1 through n in order.
+
+Person 1 starts off with a sword and kills person 2.
+He then passes the sword to the next person still standing, in this case person 3.
+Person 3 then uses the sword to kill person 4, and passes it to person 5.
+This pattern continues around and around the circle until just one person remains.
+
+What is the number of this person?
+
+Example:
+
+For n = 5, the result should be 3.
+
+1 kills 2, passes to 3.
+3 kills 4, passes to 5.
+5 kills 1, passes to 3.
+3 kills 5 and wins.
+
+Input/Output:
+
+[input] integer n
+
+The number of people. 1 through n standing in a circle.
+
+1 <= n <= 1e9
+
+[output] an integer
+
+The index of the last person standing.
+"""
+
+
+def circle_slash(n: int) -> int:
+    # Largest power of 2 <= n.
+    L = 1 << (n.bit_length() - 1)
+    return 2 * (n - L) + 1
