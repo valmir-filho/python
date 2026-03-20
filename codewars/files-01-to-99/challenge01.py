3cbd9de4b13aeb419e5e6331a579d762f8c40953
Valmir <vmcfilho@gmail.com>
2026-03-20 15:54:59 -0300

    Fix docstring formatting in challenge01.py

--- codewars/files-01-to-99/challenge01.py
+++ codewars/files-01-to-99/challenge01.py
@@ -1,4 +1,4 @@
-""
+"""
 Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also
 known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing
 every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
