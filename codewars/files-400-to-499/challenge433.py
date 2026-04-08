0964a7342a9eebadfa4643435752981c653629f6
Valmir <vmcfilho@gmail.com>
2026-04-08 08:43:23 -0300

    Create challenge433.py

---
+++ codewars/files-400-to-499/challenge433.py
@@ -0,0 +1,18 @@
+"""
+An ordered sequence of numbers from 1 to N is given.
+One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+Example:
+
+- The starting array sequence is [1,2,3,4,5,6,7,8,9];
+- The mixed array with one deleted number is [3,2,4,6,7,8,1,9];
+- Your function should return the int 5.
+
+If no number was deleted from the starting array, your function should return the int 0.
+
+Note: N may be 1 or less (in the latter case, the first array will be []).
+"""
+
+
+def find_deleted_number(arr, mixed_arr):
+    return sum(arr) - sum(mixed_arr)
