0d27b50c804a65e05183d663cc7d9b142cd35b4b
Valmir <vmcfilho@gmail.com>
2026-05-25 08:41:25 -0300

    Create challenge453.py

---
+++ codewars/files-400-to-499/challenge453.py
@@ -0,0 +1,16 @@
+"""
+Simple enough this one - you will be given an array.
+The values in the array will either be numbers or strings, or a mix of both.
+You will not get an empty array, nor a sparse one.
+
+Your job is to return a single array that has first the numbers sorted in ascending order,
+followed by the strings sorted in alphabetic order. The values must maintain their original type.
+
+Note that numbers written as strings are strings and must be sorted with the other strings.
+"""
+
+
+def db_sort(arr):
+    numbers = sorted([x for x in arr if isinstance(x, (int, float))])
+    strings = sorted([x for x in arr if isinstance(x, str)])
+    return numbers + strings
