f62a20a433b6fc5d17047ccd69ebf68af78b3ee7
Valmir <vmcfilho@gmail.com>
2026-06-01 10:20:08 -0300

    Create challenge458.py

---
+++ codewars/files-400-to-499/challenge458.py
@@ -0,0 +1,22 @@
+"""
+Task Description
+
+You're re-designing a blog, and the blog's posts have the Weekday Month Day, time format for showing the date and time when a post was made, e.g., Friday May 2, 7pm.
+
+You're running out of screen real estate, and on some pages you want to display a shorter format, Weekday Month Day that omits the time.
+
+Write a function that takes the website date/time in its original string format and returns the shortened format.
+
+Input
+
+Input will always be a string, e.g., "Friday May 2, 7pm". 
+
+Output
+
+Output will be the shortened string, e.g., "Friday May 2".
+"""
+
+
+def shorten_to_date(long_date):
+    return long_date.split(',')[0]
+  
