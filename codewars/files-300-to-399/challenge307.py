78b0468b6c5f9894aa764a2a37d34cfcda483c61
Valmir <vmcfilho@gmail.com>
2026-06-28 07:18:29 -0300

    Atualizar o challenge307.py

--- codewars/files-300-to-399/challenge307.py
+++ codewars/files-300-to-399/challenge307.py
@@ -1,4 +1,4 @@
-"""
+""
 There is a secret string which is unknown to you. Given a collection of random triplets from the string, recover the original string.
 A triplet here is defined as a sequence of three letters such that each letter occurs somewhere before the next in the given string. "whi" is a triplet for the string "whatisup".
 As a simplification, you may assume that no letter occurs more than once in the secret string.
