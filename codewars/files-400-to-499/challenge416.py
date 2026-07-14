d54c4fd1ebe7ab3ee776b60abb1482460a87c91a
Valmir <vmcfilho@gmail.com>
2026-07-14 12:20:58 -0300

    Update challenge416.py

--- codewars/files-400-to-499/challenge416.py
+++ codewars/files-400-to-499/challenge416.py
@@ -1,4 +1,4 @@
-"""
+""
 Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
 
 Valid examples, should return true:
